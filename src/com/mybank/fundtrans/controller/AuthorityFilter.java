ae901eddb2256596813fe9fd89ade5358a786e00
岭上团子 <627677955@qq.com>
2017-03-26 16:27:03 +0800

    出现\ufeff问题

---
+++ src/com/mybank/fundtrans/controller/AuthorityFilter.java
@@ -0,0 +1,55 @@
+﻿package com.mybank.fundtrans.controller;
+
+import java.io.IOException;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+//权限验证过滤器
+@WebFilter(dispatcherTypes = {DispatcherType.REQUEST }
+,urlPatterns = { "/*" })
+public class AuthorityFilter implements Filter {
+	private String logPage="/login.jsp";
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method st
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		String request_uri = req.getRequestURI();
+		String contextPath = req.getContextPath();
+		String uri = request_uri.substring(contextPath.length());
+		if (req.getSession().getAttribute("user") == null) {
+			if (uri.equals(logPage) || request_uri.indexOf("css/") > 0
+					|| request_uri.indexOf("images/") > 0
+					|| request_uri.indexOf("scripts/") > 0
+					|| uri.equals("/User")) {
+				chain.doFilter(request, response);
+				return;
+			}
+			else
+			{
+				res.sendRedirect("login.jsp");
+			}
+		}
+		else chain.doFilter(request, response);
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
--- src/com/mybank/fundtrans/controller/EncodingFilter.java
+++ src/com/mybank/fundtrans/controller/EncodingFilter.java
@@ -1,31 +1,59 @@
 package com.mybank.fundtrans.controller;
 
-import org.apache.catalina.filters.ExpiresFilter;
-
-import javax.servlet.*;
+import java.io.IOException;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
- * Created by yazawanico on 2017/3/20.
+ * Servlet Filter implementation class EncodingFilter
  */
-@WebFilter(dispatcherTypes = {DispatcherType.REQUEST},urlPatterns = {"/*"})
-
-
-//todo
+@WebFilter(dispatcherTypes = {DispatcherType.REQUEST }
+					, urlPatterns = { "/*" })
 public class EncodingFilter implements Filter {
-    public void destroy() {
+
+    /**
+     * Default constructor. 
+     */
+    public EncodingFilter() {
+        // TODO Auto-generated constructor stub
     }
 
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-       // HttpServletRequest req = (HttpServletRequest)request;
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
 
-        chain.doFilter(req, resp);
-    }
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		HttpServletRequest req=(HttpServletRequest)request;
+		HttpServletResponse res=(HttpServletResponse)response;
+		req.setCharacterEncoding("utf-8");
+		System.out.println("*********Filter is called!*******************");
+		// pass the request along the filter chain
+		//放行
+		chain.doFilter(request, response);
+	}
 
-    public void init(FilterConfig config) throws ServletException {
-
-    }
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
 
 }
